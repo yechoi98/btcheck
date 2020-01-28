4d9ca058a76b8c3eed2f61d02ad5a7c005fd2a79
youngeun <lemonjuice123@naver.com>
2020-01-28 18:49:57 +0900

    1

--- routes/home.js
+++ routes/home.js
@@ -6,4 +6,27 @@ router.get('/', function(req, res){
   res.render('home');
 });
 
+
+// Login
+router.get('/login', function (req,res) {
+  var username = req.flash('username')[0];
+  var errors = req.flash('errors')[0] || {};
+  res.render('login', {
+    username:username,
+    errors:errors
+  });
+});
+
+// Mac Address Information
+router.get('/macinfo', function(req, res){
+  res.render('macinfo');
+});
+
+
+// Forgot Password
+router.get('/forgot-password', function(req, res){
+  res.render('forgot-password');
+});
+
+
 module.exports = router;
