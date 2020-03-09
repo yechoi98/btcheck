ad9cb16e4de0ac857f99fb27052a5c65d9419591
youngeun <lemonjuice123@naver.com>
2020-03-09 18:19:14 +0900

    test

--- config/schedule.js
+++ config/schedule.js
@@ -11,7 +11,15 @@ function scheduleSubjects() {
                 let endTime = new Date(date);
                 endTime.setMinutes(endTime.getMinutes() + subject.duration) // 수업 시간 끝나면 스케줄 종료
                 schedule.scheduleJob(date, function () {
-                  console.log('hihihihi')
+                    addon.scanBluetoothDevices() // 3초 간 스캔
+                    console.log(new Date().getHours(), '시', new Date().getMinutes(), '분', new Date().getSeconds(), '초', 'scanning start!');
+                    schedule.scheduleJob({
+                        end: endTime,
+                        rule: '*/5 * * * *'
+                    }, function () { // 5분마다 반복
+                        addon.scanBluetoothDevices() // 3초 간 스캔
+                        console.log(new Date().getHours(), '시', new Date().getMinutes(), '분', new Date().getSeconds(), '초', 'scanninagain!');
+                    });
                 })
             })
         }) // end of subjects.forEach
