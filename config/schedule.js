efd11bca048ba71f6df96a6adb4f668f52eed21e
youngeun <lemonjuice123@naver.com>
2020-03-17 12:27:58 +0900

    수정

--- config/schedule.js
+++ config/schedule.js
@@ -10,126 +10,145 @@ function scheduleSubjects() {
         if (err) return res.json(err);
         subjects.forEach(function (subject) {
             subject.dates.forEach(function (date) {
-		    scheduleJob(date, subject)
+                scheduleJob(date, subject)
             }) // end of dates.forEach
         }) // end of subjects.forEach
     }) // end of Subject.find
 }
 
-function scheduleJob(date, subject){
-   const endTime = new Date(new Date(date).getTime() + (subject.duration+1)*60000);
-   let min = 0
+function scheduleJob(date, subject) {
+    const endTime = new Date(new Date(date).getTime() + (subject.duration + 1) * 60000);
+    let min = 0
     const jobStart = new cron.CronJob({
-	    cronTime: new Date(new Date(date)-1000),
-	    onTick: function(){
-	    job.start()
+        cronTime: new Date(new Date(date) - 1000),
+        onTick: function () {
+            job.start()
             console.log('schedule will be run after 1 second.')
-	    },
-	    start: true,
-	    timeZone: 'Asia/Seoul'
+        },
+        start: true,
+        timeZone: 'Asia/Seoul'
     })
 
 
-   const job = new cron.CronJob({
-	   cronTime: '0 */5 * * * *',
-	   onTick: function(){
-           console.log(new Date().toLocaleString())
-	   addon.scanBluetoothDevices()
-           compareAndSave(date, subject, min)
-	   min=min+5
-	   },
-	   start: false,
-	   timeZone: 'Asia/Seoul'
+    const job = new cron.CronJob({
+        cronTime: '0 */5 * * * *',
+        onTick: function () {
+            console.log(new Date().toLocaleString())
+            addon.scanBluetoothDevices()
+            compareAndSave(date, subject, min)
+            min = min + 5
+        },
+        start: false,
+        timeZone: 'Asia/Seoul'
 
-   })
+    })
 
-   
-   const jobEnd = new cron.CronJob({
-	    cronTime: endTime,
-	    onTick: function(){
-	    job.stop()
+
+    const jobEnd = new cron.CronJob({
+        cronTime: endTime,
+        onTick: function () {
+            job.stop()
             console.log('schedule finished at', new Date().toLocaleString())
-	    },
-	    start: true,
-	    timeZone: 'Asia/Seoul'
+        },
+        start: true,
+        timeZone: 'Asia/Seoul'
     })
 }
 
-function compareAndSave(date, subject, min){
-	const startTime = new Date(new Date(date).getTime()+min*60000)
-	const endTime = new Date(startTime.getTime()+60000)
+function compareAndSave(date, subject, min) {
+    const startTime = new Date(new Date(date).getTime() + min * 60000)
+    const endTime = new Date(startTime.getTime() + 60000)
 
-	Scan.find(function(err,scans){
-	if (err) res.json(err)
-	  scans.forEach(function(scan){
-            let scanTime = new Date(scan.time)
-	    User.find(function(err,users){
-	      if (err) res.json(err)
-	      users.forEach(function(user)){
-    if((user.job == 'student') && (user.macAddress == scan.address) && ((startTime <= scanTime) && (endTime >= scanTime)) ) {
-		Result.findOne({username:user.username, date:date}, function(err, result)){
-		  if(err) res.json(err)
-		  if(result == null) {
-		  // create
-	       
-      Result.create({
-                  name: user.name,
-                  username:user.username, 
-                  subject:subject.subject,
-                  date:_date,
-                  results:{minutes:i, time:scan.time, result:"O"}, 
-                }, function(err, result){
-                  if(err) return res.json(err);
-       })
-      break
-		  }
-		  else {
-		  // update
+    Scan.find(function (err, scans) {
+            if (err) res.json(err)
+            scans.forEach(function (scan) {
+                    let scanTime = new Date(scan.time)
+                    User.find(function (err, users) {
+                            if (err) res.json(err)
+                            users.forEach(function (user) {
+                                if ((user.job == 'student') && (user.macAddress == scan.address) && ((startTime <= scanTime) && (endTime >= scanTime))) {
+                                    Result.findOne({
+                                        username: user.username,
+                                        date: date
+                                    }, function (err, result) {
+                                        if (err) res.json(err)
+                                        if (result == null) {
+                                            // create
+                                            Result.create({
+                                                name: user.name,
+                                                username: user.username,
+                                                subject: subject.subject,
+                                                date: _date,
+                                                results: {
+                                                    minutes: min,
+                                                    time: scan.time,
+                                                    result: "O"
+                                                },
+                                            }, function (err, result) {
+                                                if (err) return res.json(err);
+                                                console.log(result)
+                                            })
+                                            break
+                                        } 
+                                        else {
+                                            // update
+                                            result.results.push({
+                                                minutes: i,
+                                                time: scan.time,
+                                                result: "O"
+                                            })
+                                            result.save()
+                                            console.log(result)
+                                            break
+                                        } // else
+                                    }) // Result.findOne
+                                } // if
+                            }) // users.forEach
+                    }) // User.find
+            }) // scans.forEach
+    }) // Scan.find
+} // function
 
-        result.results.push({minutes:i, time:scan.time, result:"O"})
-	result.save()
-        break
-		  }
-		}
-	      }
-	    })
-	  })
-	})
-    }
-}
-
-function saveResult(_date, subject, user, scan){
-  Result.findOne({username:user.username, date:_date}, function(err, result){
-  if(err) res.json(err)
-  let i = 0
-  let startTime, endTime
-   for( ; i <= subject.duration; i=i+5) {
-     startTime = new Date(new Date(_date).getTime() + i*60000) 
-     endTime = new Date(new Date(startTime).getTime()+1*60000)
-     if( (scan.time>=startTime)&&(scan.time<endTime) ){
-       if(result==null){
-      // create result 
-      Result.create({
-                  name: user.name,
-                  username:user.username, 
-                  subject:subject.subject,
-                  date:_date,
-                  results:{minutes:i, time:scan.time, result:"O"}, 
-                }, function(err, result){
-                  if(err) return res.json(err);
-       })
-      break
-      }
-      else {
-        result.results.push({minutes:i, time:scan.time, result:"O"})
-	result.save()
-        break
-      }
-     }
-   }
-  })
+function saveResult(_date, subject, user, scan) {
+    Result.findOne({
+        username: user.username,
+        date: _date
+    }, function (err, result) {
+        if (err) res.json(err)
+        let i = 0
+        let startTime, endTime
+        for (; i <= subject.duration; i = i + 5) {
+            startTime = new Date(new Date(_date).getTime() + i * 60000)
+            endTime = new Date(new Date(startTime).getTime() + 1 * 60000)
+            if ((scan.time >= startTime) && (scan.time < endTime)) {
+                if (result == null) {
+                    // create result 
+                    Result.create({
+                        name: user.name,
+                        username: user.username,
+                        subject: subject.subject,
+                        date: _date,
+                        results: {
+                            minutes: i,
+                            time: scan.time,
+                            result: "O"
+                        },
+                    }, function (err, result) {
+                        if (err) return res.json(err);
+                    })
+                    break
+                } else {
+                    result.results.push({
+                        minutes: i,
+                        time: scan.time,
+                        result: "O"
+                    })
+                    result.save()
+                    break
+                }
+            }
+        }
+    })
 }
 
 module.exports = scheduleSubjects;
-
-
