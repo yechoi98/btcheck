5cb2968d008385437c1dff0a1f23bc6e3244e845
yechoi98 <lemonjuice123@naver.com>
2020-03-17 12:18:39 +0900

    test

--- config/schedule.js
+++ config/schedule.js
@@ -17,7 +17,8 @@ function scheduleSubjects() {
 }
 
 function scheduleJob(date, subject){
-	
+   const endTime = new Date(new Date(date).getTime() + (subject.duration+1)*60000);
+   let min = 0
     const jobStart = new cron.CronJob({
 	    cronTime: new Date(new Date(date)-1000),
 	    onTick: function(){
@@ -34,14 +35,14 @@ function scheduleJob(date, subject){
 	   onTick: function(){
            console.log(new Date().toLocaleString())
 	   addon.scanBluetoothDevices()
-           compareAndSave(date, subject)
+           compareAndSave(date, subject, min)
+	   min=min+5
 	   },
 	   start: false,
 	   timeZone: 'Asia/Seoul'
 
    })
 
-   var endTime = new Date(new Date(date).getTime() + (subject.duration+1)*60000);
    
    const jobEnd = new cron.CronJob({
 	    cronTime: endTime,
@@ -54,26 +55,47 @@ function scheduleJob(date, subject){
     })
 }
 
-function compareAndSave(date, subject){
+function compareAndSave(date, subject, min){
+	const startTime = new Date(new Date(date).getTime()+min*60000)
+	const endTime = new Date(startTime.getTime()+60000)
 
-User.find(function(err, _users){
-if (err) return res.json(err)
-let users = _users
-Scan.find(function(err, _scans){
-if (err) return res.json(err)
-let scans=_scans
-scans.forEach(function(scan){
-  users.forEach(function(user){
-     const startTime = new Date(date)
-     const scanTime = new Date(scan.time)
-     const endTime = new Date(new Date(startTime).getTime()+subject.duration*60000)
-    if((user.job == 'student') && (user.macAddress == scan.address) && ((startTime <= scanTime) && (endTime >= scanTime)) ) { // if MAC address matches and scanned time is between class start time and class end time.
-	    saveResult(date, subject, user, scan) 
+	Scan.find(function(err,scans){
+	if (err) res.json(err)
+	  scans.forEach(function(scan){
+            let scanTime = new Date(scan.time)
+	    User.find(function(err,users){
+	      if (err) res.json(err)
+	      users.forEach(function(user)){
+    if((user.job == 'student') && (user.macAddress == scan.address) && ((startTime <= scanTime) && (endTime >= scanTime)) ) {
+		Result.findOne({username:user.username, date:date}, function(err, result)){
+		  if(err) res.json(err)
+		  if(result == null) {
+		  // create
+	       
+      Result.create({
+                  name: user.name,
+                  username:user.username, 
+                  subject:subject.subject,
+                  date:_date,
+                  results:{minutes:i, time:scan.time, result:"O"}, 
+                }, function(err, result){
+                  if(err) return res.json(err);
+       })
+      break
+		  }
+		  else {
+		  // update
+
+        result.results.push({minutes:i, time:scan.time, result:"O"})
+	result.save()
+        break
+		  }
+		}
+	      }
+	    })
+	  })
+	})
     }
-  })
-})	
-})
-})
 }
 
 function saveResult(_date, subject, user, scan){
